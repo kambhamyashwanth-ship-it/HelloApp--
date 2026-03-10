23d110c57ed3c61077fc4013dd81e29a32b4ec88
kambhamyashwanth-ship-it <kambhamyashwanth@gmail.com>
2026-03-10 14:12:04 +0530

    Modified again

--- HelloApp.java
+++ HelloApp.java
@@ -3,6 +3,6 @@ public static void main(String[] args){
 //variables & datatypes
 int num = 100;
 System.out.println(num);
-System.out.println("Hello All");
+System.out.println("Hello All step classes are boring af bro srsly");
 }
 }
